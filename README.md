49ad4cba3c71be67e1cc35ba32259106f43379e1
18446038+Ebycow@users.noreply.github.com <18446038+Ebycow@users.noreply.github.com>
2020-03-10 23:33:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## setting.json
+```
+{
+    "update" : "0 */30 * * * *",
+    "accessToken" : "",
+    "accessTokenSecret" : "",
+    "resourceId" : "",
+    "recordNames" : [
+        "www"
+    ],
+    "appraiser" : "http://...( IPv4 address of text/plain )"
+}
+```
---
+++ index.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+const axios = require('axios');
+const cron = require('node-cron');
+
+const setting = JSON.parse(fs.readFileSync('./setting.json'));
+
+const updateCronExpression = setting.update;
+
+const accessToken = setting.accessToken;
+const accessTokenSecret = setting.accessTokenSecret;
+
+const resourceId = setting.resourceId;
+
+const apiRoot = 'https://secure.sakura.ad.jp/cloud/zone/tk1a/api/cloud/1.1/';
+
+const recordNames = setting.recordNames;
+
+const appraiser = setting.appraiser;
+
+async function getIpv4Address() {
+    const response = await axios.get(appraiser,{
+        auth: {
+            username: accessToken,
+            password: accessTokenSecret
+        }
+    });
+
+    return response.data;
+}
+
+async function getRecordSets() {
+    const response = await axios.get(apiRoot + 'commonserviceitem/' + resourceId,{
+        auth: {
+            username: accessToken,
+            password: accessTokenSecret
+        }
+    });
+    return response.data.CommonServiceItem.Settings.DNS.ResourceRecordSets;
+}
+
+function changeRecordSets(recordSets, address) {
+    for (const recordName of recordNames) {
+        recordSets.map((rcd) => {
+            if(rcd.Name === recordName) {
+                rcd.RData = address;
+            }
+        });
+    }
+
+    return recordSets;
+
+}
+
+async function putSetting(recordSets) {
+    const data = {
+        "CommonServiceItem" : {
+            "Settings" : {
+                "DNS" : {
+                    "ResourceRecordSets" : recordSets
+                }
+            }
+        }
+    }
+
+    const response = await axios({
+        method: 'put',
+        url: apiRoot + 'commonserviceitem/' + resourceId,
+        auth: {
+            username: accessToken,
+            password: accessTokenSecret
+        },
+        data: data
+        
+    });
+
+    return response.data;
+
+}
+
+(async() => {
+    let address = await getIpv4Address();
+    console.log(new Date, "address=", address)
+    let recordSets = await getRecordSets();
+
+    recordSets = changeRecordSets(recordSets, address);
+    console.log('update...');
+    await putSetting(recordSets);
+    console.log('done!');
+
+    cron.schedule(updateCronExpression, async() => {
+        const currentAddress = await getIpv4Address();
+        console.log(new Date, "address=", currentAddress)
+        if(address !== currentAddress){
+            address = currentAddress;
+            console.log('update...');
+            recordSets = changeRecordSets(recordSets, address);
+            await putSetting(recordSets);
+            console.log('done!');
+        }
+    });
+    
+})();
